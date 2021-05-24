526c478533845d846a3690115df67d053a5f8781
Vadym Shevchenko <shev4enko.vadym@gmail.com>
2021-05-24 13:31:51 +0300

    feat: hiperlinks as icons

--- animate_steps.js
+++ animate_steps.js
@@ -1,23 +1,23 @@
 function main() {
-  $(".blobs").click(function() {
-    $("#middle").addClass("animated");     
-    $("#blob1").addClass("animated");     
-    $("#blob2").addClass("animated");
-  })
-  
-  $("#blob1").bind("webkitAnimationEnd mozAnimationEnd animationend", function(){  
-    $("#blob1").removeClass("animatedR");  
-    $("#blob2").removeClass("animatedR");  
+  $('.blobs').click(function () {
+    $('#middle').addClass('animated');
+    $('#blob1').addClass('animated');
+    $('#blob2').addClass('animated');
+    $('.blobs').addClass('float')
   })
 
-  $(".blobs .blob").on('click', function(e) {
+  $('#blob1').bind('webkitAnimationEnd mozAnimationEnd animationend', function () {
+    $('#blob1').removeClass('animatedR');
+    $('#blob2').removeClass('animatedR');
+  })
+
+  $('.blobs .blob').on('click', function (e) {
     const baseArticleName = '.info-content'
     $('.info-content article').map((ind, el) => {
       el.classList.remove('active')
     });
     $(`${baseArticleName}-${e.target.name}`).addClass('active');
   })
-
 }
 
 window.addEventListener('load', function () {
